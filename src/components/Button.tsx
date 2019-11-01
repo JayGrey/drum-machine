1820889bda3aeadb164fa4795c5ac5abf3e43685
Kulishenko Igor <KulishenkoIV@oschadbank.ua>
2019-11-01 16:35:51 +0200

    keypress processing wip

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,12 +1,18 @@
 import * as React from "react";
 
+
 // () => props.clickHandler(`${props.title} player`)
 
 export const Button = (props: { title: string; clickHandler: Function }) => {
+  const handler = () => {
+    props.clickHandler(`${props.title} played`);
+    (document.querySelector(`audio#${props.title}`) as HTMLMediaElement).play();
+  }
+
   return (
     <div className="col d-flex flex-column align-items-center ">
       <span className="border border-secondary rounded p-1">
-        <button className="btn btn-secondary drum-pad" onClick={() => props.clickHandler(`${props.title} player`)}>
+        <button className="btn btn-secondary drum-pad" onClick={handler}>
           <audio src={`assets/${props.title}.wav`} className="clip" id={props.title}></audio>
         </button>
       </span>
--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import { useEffect } from "react";
 import { Provider } from "react-redux";
 
 import Display from "./Display";
@@ -7,6 +8,17 @@ import Pinpad from "./Pinpad";
 import { store } from "../store/store";
 
 export const App = () => {
+
+  const keyHandler = (event: KeyboardEvent) => {
+    console.log(event.key, event.code);
+  }
+
+  useEffect(() => {
+    document.addEventListener("keypress", keyHandler);
+
+    return () => document.removeEventListener("keypress", keyHandler);
+  });
+
   return (
     <Provider store={store}>
       <div id="drum-machine" className="container-fluid">
